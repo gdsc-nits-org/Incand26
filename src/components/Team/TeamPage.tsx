876edcbc84934a138caef884e8333b8508e8dd8b
DhrubaAgarwalla <dhrubagarwala67@gmail.com>
2026-01-12 21:41:36 +0530

    team-details-update

--- src/components/Team/TeamPage.tsx
+++ src/components/Team/TeamPage.tsx
@@ -2,96 +2,92 @@
 
 import { MemberCard } from "./MemberCard";
 
-// Core team member data
+// Core team member data - Heads (4th Year)
 const coreMembers = [
     {
         maskGroup: "/team/mask-group.webp",
         frameImage: "/team/e45fa6e1f8e8c01193d414257a13c3d2-1-5.webp",
         profileImage: "/team/d49ca87acb58b4eef86bba9825321fae-2-5.webp",
         socialIcons: [
-            { maskGroup: "/team/mask-group-1.webp", icon: "/team/fb.webp", link: "#" },
-            { maskGroup: "/team/mask-group-2.webp", icon: "/team/github.webp", link: "#" },
-            { maskGroup: "/team/mask-group-3.webp", icon: "/team/linkedin.webp", link: "#" },
+            { maskGroup: "/team/mask-group-1.webp", icon: "/team/insta.svg", link: "https://www.instagram.com/_samarjitroy_/" },
+            { maskGroup: "/team/mask-group-2.webp", icon: "/team/github.webp", link: "https://github.com/Samarjit25" },
+            { maskGroup: "/team/mask-group-3.webp", icon: "/team/linkedin.webp", link: "https://www.linkedin.com/in/samarjit-roy-368071257/" },
         ],
-        name: "Hibino Kafka",
-        role: "UI/UX Designer",
+        name: "Samarjit Roy",
+        role: "UI/UX Head",
     },
     {
         maskGroup: "/team/mask-group.webp",
         frameImage: "/team/e45fa6e1f8e8c01193d414257a13c3d2-1-5.webp",
         profileImage: "/team/d49ca87acb58b4eef86bba9825321fae-2-5.webp",
         socialIcons: [
-            { maskGroup: "/team/mask-group-1.webp", icon: "/team/fb.webp", link: "#" },
-            { maskGroup: "/team/mask-group-2.webp", icon: "/team/github.webp", link: "#" },
+            { maskGroup: "/team/mask-group-1.webp", icon: "/team/insta.svg", link: "#" },
+            { maskGroup: "/team/mask-group-2.webp", icon: "/team/github.webp", link: "https://github.com/rishinhk004" },
             { maskGroup: "/team/mask-group-3.webp", icon: "/team/linkedin.webp", link: "#" },
         ],
-        name: "Hibino Kafka",
-        role: "UI/UX Designer",
+        name: "Rishi Raj Saha",
+        role: "Web Head",
     },
     {
         maskGroup: "/team/mask-group.webp",
         frameImage: "/team/e45fa6e1f8e8c01193d414257a13c3d2-1-5.webp",
         profileImage: "/team/d49ca87acb58b4eef86bba9825321fae-2-5.webp",
         socialIcons: [
-            { maskGroup: "/team/mask-group-1.webp", icon: "/team/fb.webp", link: "#" },
+            { maskGroup: "/team/mask-group-1.webp", icon: "/team/insta.svg", link: "#" },
             { maskGroup: "/team/mask-group-2.webp", icon: "/team/github.webp", link: "#" },
-            { maskGroup: "/team/mask-group-3.webp", icon: "/team/linkedin.webp", link: "#" },
+            { maskGroup: "/team/mask-group-3.webp", icon: "/team/linkedin.webp", link: "https://www.linkedin.com/in/siddharth-ghosh-18ba29251/" },
         ],
-        name: "Hibino Kafka",
-        role: "UI/UX Designer",
+        name: "Siddharth Ghosh",
+        role: "UI/UX Head",
     },
     {
         maskGroup: "/team/mask-group.webp",
         frameImage: "/team/e45fa6e1f8e8c01193d414257a13c3d2-1-5.webp",
         profileImage: "/team/d49ca87acb58b4eef86bba9825321fae-2-5.webp",
         socialIcons: [
-            { maskGroup: "/team/mask-group-1.webp", icon: "/team/fb.webp", link: "#" },
-            { maskGroup: "/team/mask-group-2.webp", icon: "/team/github.webp", link: "#" },
-            { maskGroup: "/team/mask-group-3.webp", icon: "/team/linkedin.webp", link: "#" },
+            { maskGroup: "/team/mask-group-1.webp", icon: "/team/insta.svg", link: "#" },
+            { maskGroup: "/team/mask-group-2.webp", icon: "/team/github.webp", link: "https://github.com/barnilsarma" },
+            { maskGroup: "/team/mask-group-3.webp", icon: "/team/linkedin.webp", link: "https://www.linkedin.com/in/barnil-sarma-34383a255/" },
         ],
-        name: "Hibino Kafka",
-        role: "UI/UX Designer",
+        name: "Barnil Sarma",
+        role: "Web Head",
     },
     {
         maskGroup: "/team/mask-group.webp",
         frameImage: "/team/e45fa6e1f8e8c01193d414257a13c3d2-1-5.webp",
         profileImage: "/team/d49ca87acb58b4eef86bba9825321fae-2-5.webp",
         socialIcons: [
-            { maskGroup: "/team/mask-group-1.webp", icon: "/team/fb.webp", link: "#" },
+            { maskGroup: "/team/mask-group-1.webp", icon: "/team/insta.svg", link: "https://www.instagram.com/dev.kaustav" },
             { maskGroup: "/team/mask-group-2.webp", icon: "/team/github.webp", link: "#" },
-            { maskGroup: "/team/mask-group-3.webp", icon: "/team/linkedin.webp", link: "#" },
+            { maskGroup: "/team/mask-group-3.webp", icon: "/team/linkedin.webp", link: "https://www.linkedin.com/in/kaustav-dev-429123248" },
         ],
-        name: "Hibino Kafka",
-        role: "UI/UX Designer",
-    },
-    {
-        maskGroup: "/team/mask-group.webp",
-        frameImage: "/team/e45fa6e1f8e8c01193d414257a13c3d2-1-5.webp",
-        profileImage: "/team/d49ca87acb58b4eef86bba9825321fae-2-5.webp",
-        socialIcons: [
-            { maskGroup: "/team/mask-group-1.webp", icon: "/team/fb.webp", link: "#" },
-            { maskGroup: "/team/mask-group-2.webp", icon: "/team/github.webp", link: "#" },
-            { maskGroup: "/team/mask-group-3.webp", icon: "/team/linkedin.webp", link: "#" },
-        ],
-        name: "Hibino Kafka",
-        role: "UI/UX Designer",
+        name: "Kaustav Dev",
+        role: "UI/UX Head",
     },
 ];
 
-// Junior team member data
+// Junior team member data - Co-Heads (3rd Year) and Members (2nd Year)
 const juniorMembers = [
-    { id: 1, name: "Surojit Roy", role: "UI/UX designer", image: "/team/d49ca87acb58b4eef86bba9825321fae-2-5.webp" },
-    { id: 2, name: "Surojit Roy", role: "UI/UX designer", image: "/team/d49ca87acb58b4eef86bba9825321fae-2-5.webp" },
-    { id: 3, name: "Surojit Roy", role: "UI/UX designer", image: "/team/d49ca87acb58b4eef86bba9825321fae-2-5.webp" },
-    { id: 4, name: "Surojit Roy", role: "UI/UX designer", image: "/team/d49ca87acb58b4eef86bba9825321fae-2-5.webp" },
-    { id: 5, name: "Surojit Roy", role: "UI/UX designer", image: "/team/d49ca87acb58b4eef86bba9825321fae-2-5.webp" },
-    { id: 6, name: "Surojit Roy", role: "UI/UX designer", image: "/team/d49ca87acb58b4eef86bba9825321fae-2-5.webp" },
-    { id: 7, name: "Surojit Roy", role: "UI/UX designer", image: "/team/d49ca87acb58b4eef86bba9825321fae-2-5.webp" },
-    { id: 8, name: "Surojit Roy", role: "UI/UX designer", image: "/team/d49ca87acb58b4eef86bba9825321fae-2-5.webp" },
-    { id: 9, name: "Surojit Roy", role: "UI/UX designer", image: "/team/d49ca87acb58b4eef86bba9825321fae-2-5.webp" },
-    { id: 10, name: "Surojit Roy", role: "UI/UX designer", image: "/team/d49ca87acb58b4eef86bba9825321fae-2-5.webp" },
-    { id: 11, name: "Surojit Roy", role: "UI/UX designer", image: "/team/d49ca87acb58b4eef86bba9825321fae-2-5.webp" },
-    { id: 12, name: "Surojit Roy", role: "UI/UX designer", image: "/team/d49ca87acb58b4eef86bba9825321fae-2-5.webp" },
+    // Co-Heads (3rd Year)
+    { id: 1, name: "Jitamanyu Phukan", role: "UI/UX Co-Head", image: "/team/d49ca87acb58b4eef86bba9825321fae-2-5.webp", linkedin: "https://www.linkedin.com/in/jitamanyu-phukan-562728280", github: "#", instagram: "#" },
+    { id: 2, name: "Piyush Chatterjee", role: "UI/UX Co-Head", image: "/team/d49ca87acb58b4eef86bba9825321fae-2-5.webp", linkedin: "https://www.linkedin.com/in/piyush-chatterjee64/", github: "#", instagram: "#" },
+    { id: 3, name: "Bishal Das", role: "Web Co-Head", image: "/team/d49ca87acb58b4eef86bba9825321fae-2-5.webp", linkedin: "https://www.linkedin.com/in/bishalnits27", github: "https://github.com/Bishal-NITS-2003", instagram: "https://www.instagram.com/bishalnits27" },
+    { id: 4, name: "Subhajyoti Dey", role: "Web Co-Head", image: "/team/d49ca87acb58b4eef86bba9825321fae-2-5.webp", linkedin: "https://www.linkedin.com/in/subhajyoti-dey-635922235", github: "https://github.com/SubhaNITS150", instagram: "#" },
+    { id: 5, name: "Anirban Bora", role: "Web Co-Head", image: "/team/d49ca87acb58b4eef86bba9825321fae-2-5.webp", linkedin: "https://www.linkedin.com/in/anirban-bora-432b2a280", github: "https://github.com/A-nirvana", instagram: "https://www.instagram.com/ani_r_baan" },
+    { id: 6, name: "Mriganka Dey", role: "Web Co-Head", image: "/team/d49ca87acb58b4eef86bba9825321fae-2-5.webp", linkedin: "https://www.linkedin.com/in/mrigankadey/", github: "https://github.com/velgardey", instagram: "https://www.instagram.com/velgardey/" },
+    { id: 7, name: "Priyanshu Kashyap", role: "Web Co-Head", image: "/team/d49ca87acb58b4eef86bba9825321fae-2-5.webp", linkedin: "https://www.linkedin.com/in/priyanshu-kashyap-8196a928a", github: "https://github.com/7rikster", instagram: "https://www.instagram.com/priyanshu._.kashyap_" },
+    // Members (2nd Year)
+    { id: 8, name: "Kallul Gogoi", role: "Web Member", image: "/team/d49ca87acb58b4eef86bba9825321fae-2-5.webp", linkedin: "https://www.linkedin.com/in/kallul-gogoi-00a5152a0/", github: "https://github.com/kallulgogoi", instagram: "https://www.instagram.com/kallul_gogoi33/" },
+    { id: 9, name: "Swarup Das", role: "Web Member", image: "/team/d49ca87acb58b4eef86bba9825321fae-2-5.webp", linkedin: "https://www.linkedin.com/in/swarup81", github: "https://github.com/swarup081", instagram: "https://www.instagram.com/swarup_81/" },
+    { id: 10, name: "Swastika Paul", role: "Web Member", image: "/team/d49ca87acb58b4eef86bba9825321fae-2-5.webp", linkedin: "https://www.linkedin.com/in/swastika-paul-05186a381", github: "https://github.com/swastika-paul", instagram: "#" },
+    { id: 11, name: "Mrigakhi Kashyap", role: "UI/UX Member", image: "/team/d49ca87acb58b4eef86bba9825321fae-2-5.webp", linkedin: "https://www.linkedin.com/in/mrigakhi-kashyap-68188a354", github: "https://github.com/mrigakhi65", instagram: "https://www.instagram.com/_mrigakhiiiii_" },
+    { id: 12, name: "Dishank Choudhury", role: "Web Member", image: "/team/d49ca87acb58b4eef86bba9825321fae-2-5.webp", linkedin: "https://www.linkedin.com/in/dishank-choudhury-56b565318", github: "https://github.com/di35117", instagram: "https://www.instagram.com/dishank_35117" },
+    { id: 13, name: "Mayur Kalita", role: "UI/UX Member", image: "/team/d49ca87acb58b4eef86bba9825321fae-2-5.webp", linkedin: "https://www.linkedin.com/in/mayur-kalita", github: "https://github.com/mayurkalita", instagram: "https://www.instagram.com/mayurrr.design" },
+    { id: 14, name: "Harshit Agarwal", role: "Web Member", image: "/team/d49ca87acb58b4eef86bba9825321fae-2-5.webp", linkedin: "https://www.linkedin.com/in/harshit-agarwal-a119a4332/", github: "https://github.com/agarwal-harshit00", instagram: "https://www.instagram.com/harshitaga_01/" },
+    { id: 15, name: "Anusna Pradhan", role: "UI/UX Member", image: "/team/d49ca87acb58b4eef86bba9825321fae-2-5.webp", linkedin: "https://www.linkedin.com/in/anusna-pradhan/", github: "https://github.com/iz4nam1", instagram: "#" },
+    { id: 16, name: "Purba Pratim Mahanta", role: "Web Member", image: "/team/d49ca87acb58b4eef86bba9825321fae-2-5.webp", linkedin: "https://www.linkedin.com/in/purba-pratim-mahanta-7037a1358", github: "https://github.com/Purbapratim36", instagram: "#" },
+    { id: 17, name: "Priyadarkshina Khamon", role: "UI/UX Member", image: "/team/d49ca87acb58b4eef86bba9825321fae-2-5.webp", linkedin: "https://www.linkedin.com/in/priyadarkshina-khamon-a89b81332", github: "https://github.com/priyadarkshina19", instagram: "#" },
+    { id: 18, name: "Dhruba Kumar Agarwalla", role: "Web Member", image: "/team/d49ca87acb58b4eef86bba9825321fae-2-5.webp", linkedin: "https://www.linkedin.com/in/dhruba-kumar-agarwalla-7a5346270/", github: "https://github.com/DhrubaAgarwalla", instagram: "#" },
 ];
 
 export const TeamPage = () => {
@@ -172,7 +168,7 @@ export const TeamPage = () => {
                     {/* CORE MEMBERS Title */}
                     <div id="core-team" className="inline-flex items-center justify-center px-[5vw] py-[3vw] lg:px-[3.3vw] lg:py-[1.8vw] bg-[#e2be36] rounded-[2vw] lg:rounded-[0.7vw] border-[1vw] lg:border-[0.4vw] border-solid border-[#514114] mb-[10vw] lg:mb-[5vw]">
                         <div className="font-hitchcut font-normal text-[#160f0c] text-[7vw] lg:text-[4.4vw] text-justify tracking-[0.17vw] leading-none whitespace-nowrap">
-                            CORE MEMBERS
+                            TECH TEAM
                         </div>
                     </div>
 
@@ -184,11 +180,11 @@ export const TeamPage = () => {
                     </div>
 
                     {/* Heads Grid */}
-                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-[15vw] gap-y-[15vw] lg:gap-y-[5vw] mb-[20vw] lg:mb-[10vw] px-[4vw]">
+                    <div className="flex flex-wrap justify-center gap-y-[15vw] md:gap-x-[5vw] lg:gap-x-[2vw] lg:gap-y-[8vw] mb-[20vw] lg:mb-[10vw] px-[4vw] w-full max-w-[1600px]">
                         {coreMembers.map((member, index) => (
                             <div
                                 key={`member-${index}`}
-                                className={`flex justify-center ${index % 3 === 1 ? "lg:-mt-[5vw]" : ""}`}
+                                className={`flex justify-center w-full md:w-[45%] lg:w-[30%] ${index === 1 ? "lg:-mt-[5vw]" : ""}`}
                             >
                                 <MemberCard {...member} />
                             </div>
@@ -196,7 +192,7 @@ export const TeamPage = () => {
                     </div>
 
                     {/* Junior Members Title */}
-                    <div id="junior-team" className="inline-flex items-center justify-center px-[6vw] py-[3vw] lg:px-[2.3vw] lg:py-[1.2vw] bg-[#DD7A1E] rounded-[1.5vw] lg:rounded-[0.5vw] border-[0.8vw] lg:border-[0.3vw] border-solid border-black mb-[10vw] lg:mb-[5vw]">
+                    <div id="junior-team" className="inline-flex items-center justify-center px-[6vw] py-[3vw] lg:px-[2.3vw] lg:py-[1.2vw] bg-[#e2be36] rounded-[1.5vw] lg:rounded-[0.5vw] border-[0.8vw] lg:border-[0.3vw] border-solid border-black mb-[10vw] lg:mb-[5vw]">
                         <div className="font-hitchcut font-normal text-[#160f0c] text-[8vw] lg:text-[3vw] text-justify tracking-[0.12vw] leading-none whitespace-nowrap">
                             Junior Members
                         </div>
@@ -228,14 +224,14 @@ export const TeamPage = () => {
 
                                             {/* Social Links Slide-up */}
                                             <div className="absolute bottom-[8%] left-0 w-full flex justify-center gap-[4vw] md:gap-[2vw] lg:gap-[1.5vw] z-20 transition-transform duration-300 translate-y-0 lg:translate-y-[300%] lg:group-hover:translate-y-0">
-                                                <a href="#" className="w-[8vw] h-[8vw] md:w-[4vw] md:h-[4vw] lg:w-[2.5vw] lg:h-[2.5vw] bg-black rounded-full flex items-center justify-center hover:scale-110 transition-transform">
+                                                <a href={member.linkedin} target="_blank" rel="noopener noreferrer" className="w-[8vw] h-[8vw] md:w-[4vw] md:h-[4vw] lg:w-[2.5vw] lg:h-[2.5vw] bg-black rounded-full flex items-center justify-center hover:scale-110 transition-transform">
                                                     <img src="/team/linkedin.webp" alt="LinkedIn" className="w-[60%] h-[60%] object-contain" />
                                                 </a>
-                                                <a href="#" className="w-[8vw] h-[8vw] md:w-[4vw] md:h-[4vw] lg:w-[2.5vw] lg:h-[2.5vw] bg-black rounded-full flex items-center justify-center hover:scale-110 transition-transform">
-                                                    <img src="/team/fb.webp" alt="Facebook" className="w-[60%] h-[60%] object-contain" />
-                                                </a>
-                                                <a href="#" className="w-[8vw] h-[8vw] md:w-[4vw] md:h-[4vw] lg:w-[2.5vw] lg:h-[2.5vw] bg-black rounded-full flex items-center justify-center hover:scale-110 transition-transform">
+                                                <a href={member.github} target="_blank" rel="noopener noreferrer" className="w-[8vw] h-[8vw] md:w-[4vw] md:h-[4vw] lg:w-[2.5vw] lg:h-[2.5vw] bg-black rounded-full flex items-center justify-center hover:scale-110 transition-transform">
                                                     <img src="/team/github.webp" alt="Github" className="w-[60%] h-[60%] object-contain" />
+                                                </a>
+                                                <a href={member.instagram} target="_blank" rel="noopener noreferrer" className="w-[8vw] h-[8vw] md:w-[4vw] md:h-[4vw] lg:w-[2.5vw] lg:h-[2.5vw] bg-black rounded-full flex items-center justify-center hover:scale-110 transition-transform">
+                                                    <img src="/team/insta.svg" alt="Instagram" className="w-[60%] h-[60%] object-contain" />
                                                 </a>
                                             </div>
                                         </div>
